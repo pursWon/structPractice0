f22143925da02334a385b3e419a8d6df86e6eb12
Wongi <hpurs95@gmail.com>
2022-07-29 10:36:38 +0900

    Initial Commit

---
+++ structPractice0/structPractice0App.swift
@@ -0,0 +1,17 @@
+//
+//  structPractice0App.swift
+//  structPractice0
+//
+//  Created by MacBook Air on 2022/07/29.
+//
+
+import SwiftUI
+
+@main
+struct structPractice0App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
